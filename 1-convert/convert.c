2b62ece19d8aa6ac8636d61a45439b7c55c2b3af
Harrison Joseph Saliba <salibah@wwu.edu>
2019-10-01 09:54:31 -0700

    Structure and user input configured

--- 1-convert/convert.c
+++ 1-convert/convert.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
-void convertBinary(int in, char out[]);
-void convertHex(int in, char out[]);
+void convertBinary(int in);
+void convertHex(int in);
+void printUsage();
 /* Main
 * argc, argv   Command line arguments.
 * return       zero for success, non-zero for failure
@@ -9,13 +11,22 @@ void convertHex(int in, char out[]);
 * This is the main entry point for the program.  It all starts here. 
 */
 int main(int argc, char* argv[]) {
-    printf("%s says, \"Hello, World!\"\n", argv[0]);
-
-    char out[33];
-    convertBinary(1, out);
-    convertHex(1, out);
-
-    puts(out);
+    if (argc != 2) {
+        printUsage();
+    }
+    else if ( strcmp("-x", argv[1]) == 0 ) {
+        printf("Ah ha! You want me to convert to hex! Wouldn't it be cool if I could do that?\n");
+        int input[100];
+        while(scanf("%d", input) != EOF) {
+            convertHex(input[0]);
+        }
+    }
+    else if ( strcmp("-b", argv[1]) == 0 ) {
+        printf("Ah ha! You want me to convert to binary! Wouldn't it be cool if I could do that?\n");
+    }
+    else {
+        printUsage();
+    }
 
     return 0;
 }
@@ -23,13 +34,20 @@ int main(int argc, char* argv[]) {
  * in, input number in decimal
  * out, output char array
  */
-void convertBinary(int in, char out[]) {
+void convertBinary(int in) {
     printf("I'm the convert binary function!\n");
+    // TODO: Figure out how to count the number of binary digits.  Clauson on whiteboard way? or shifts & 1? Probably the later bc its in the rubric.
 }
 /* convertHex
  * in, input number in decimal
  * out, output char array
  */
-void convertHex(int in, char out[]) {
+void convertHex(int in) {
     printf("I'm the convert hexadecimal function!\n");
+}
+/* printUage
+ * Simply prints the usage for the program
+ */
+void printUsage() {
+    printf("Usage: ./convert [-x|-b]\n");
 }
