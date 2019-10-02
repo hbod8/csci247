2b228e8d6413c97c711ab65675402611bf136e7d
Harrison Saliba <harrysaliba4139@gmail.com>
2019-10-02 10:51:43 -0700

    Correct documentaion.

--- 1-convert/convert.c
+++ 1-convert/convert.c
@@ -5,24 +5,25 @@ void convertBinary(int in);
 void convertHex(int in);
 void printUsage();
 /* Main
-* argc, argv   Command line arguments.
-* return       zero for success, non-zero for failure
-*
-* This is the main entry point for the program.  It all starts here. 
-*/
+ * argc, argv   Command line arguments.
+ * return       zero for success, non-zero for failure
+ *
+ * This is the main entry point for the program.  It all starts here.
+ * The main use of this function is to prcess command-line input and keyboard input while keyboard input is not the end of file character.
+ * -b -> binary converter
+ * -x -> hexadecimal converter
+ */
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         printUsage();
     }
     else if ( strcmp("-x", argv[1]) == 0 ) {
-        // printf("Ah ha! You want me to convert to hex! Wouldn't it be cool if I could do that?\n");
         int input[100];
         while(scanf("%d", input) != EOF) {
             convertHex(input[0]);
         }
     }
     else if ( strcmp("-b", argv[1]) == 0 ) {
-        // printf("Ah ha! You want me to convert to binary! Wouldn't it be cool if I could do that?\n");
         int input[100];
         while(scanf("%d", input) != EOF) {
             convertBinary(input[0]);
@@ -36,19 +37,16 @@ int main(int argc, char* argv[]) {
 }
 /* convertBinary
  * in, input number in decimal
- * out, output char array
+ * Converts user specified decimal number to a binary number, with spaces every nibble, and prints result to command-line.
+ * It places every binary digit in an array to ensure the result is little endian. (could be more efficient if you read the largest bit then trim zeros)
  */
 void convertBinary(int in) {
-    // printf("I'm the convert binary function!\n");
-    // TODO: Figure out how to count the number of binary digits.  Clauson on whiteboard way? or shifts & 1? Probably the later bc its in the rubric.
     char bin[] = {'0', '1'};
     char num[100];
     int i = 0;
     int a = in;
-    // printf("should be: %x\n", in);
     while (a > 0) {
-        num[i] = bin[(in >> i) & 0x01]; // 00000001 <- SHIFT THIS!
-        // printf("%c", bin[(in >> i) & 0x01]);
+        num[i] = bin[(in >> i) & 0x01]; // 00000001
         i = i + 1;
         a = a / 2;
     }
@@ -58,20 +56,20 @@ void convertBinary(int in) {
         }
         i = i - 1;
         printf("%c", num[i]);
-        // printf("(%x)", num[i]);
     }
     printf("\n");
 }
 /* convertHex
  * in, input number in decimal
- * out, output char array
+ * Converts specified decimal number to hexadeimal number, and prints result to command line.
+ * Doesnt print anything if the number is larger than the max int size of 2147483647.
  */
 void convertHex(int in) {
-    // printf("I'm the convert hexadecimal function!\n");
     char hex[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-    char num[100]; // if the digits exceed 100, there be dragons :(
+    char num[100];
     int i = 0;
-    int a = in; // bad things happen if you exceed 2147483647
+    int a = in;
+    printf("0x");
     while (a > 0) {
         num[i/4] = hex[(in >> i) & 0x0f]; // 00001111
         i = i + 4;
@@ -84,7 +82,7 @@ void convertHex(int in) {
     printf("\n");
 }
 /* printUage
- * Simply prints the usage for the program
+ * Simply prints the usage for the program in command line
  */
 void printUsage() {
     printf("Usage: ./convert [-x|-b]\n");
