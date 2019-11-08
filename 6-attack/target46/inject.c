fa4d1a307132dbcac612b2927361a8c8a44f7160
Harrison Joseph Saliba <salibah@wwu.edu>
2019-11-07 20:00:10 -0800

    Phase 1 & 1

---
+++ 6-attack/target46/inject.c
@@ -0,0 +1,10 @@
+int touch2(long d);
+
+int main() {
+    touch2(0x69afe3c6);
+    return 0;
+}
+
+int touch2(long d) {
+    return 0;
+}
---
+++ 6-attack/target46/inject.s
@@ -0,0 +1,4 @@
+.global main
+main:
+mov $0x69afe3c6,%rdi
+ret
